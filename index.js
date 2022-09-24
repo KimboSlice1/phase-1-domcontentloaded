d2d39f7143c5d73cde7333194137cba798b23d2d
Kimber Herron <kh582013@gmail.com>
2022-09-24 17:19:23 -0400

    commit tree 0436529e6e016ffd7265872d3e54955e4e0db709

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+//function updateText(text){
+    //let p = document.querySelector('p')
+    //p.textContent = text
+//}
+
+document.addEventListener("DOMContentLoaded", 
+function() { 
+    document.getElementById('text').textContent = 'This is really cool!'
+});
+   
+
+//document.querySelector('p')
